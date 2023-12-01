242f983ebbbf5f97eb30cb5f5757836fbd4d41bb
Aileen Rae <aileenlrae@gmail.com>
2023-12-01 22:25:54 +0000

    It works for the test data

--- day-01/index.js
+++ day-01/index.js
@@ -3,22 +3,28 @@ const fs = require("node:fs");
 function parse() {
   try {
     const rawData = fs.readFileSync("./day-01/test-input.txt", "utf8");
-    return rawData.split("\n");
+    return rawData.split("\n").filter(x => x !== "");
   } catch (err) {
     console.error("Error reading input:", err);
   }
 }
 
+function getFirstAndLastDigits(line) {
+    const digits = line.match(/\d+/g);
+    return Number(digits.at(0) + digits.at(-1));
+};
+
 function part1(data) {
-    return data;
+    const result = data.reduce((acc, curr) => acc + getFirstAndLastDigits(curr), 0);
+    return result;
 }
 
 function run() {
   const data = parse();
   const part1Solution = part1(data);
-  console.log(part1Solution);
+  console.log("Part 1 solution:", part1Solution);
 }
 
 run();
 
-module.exports = {parse, part1};
+module.exports = {parse, getFirstAndLastDigits, part1};
--- day-01/index.test.js
+++ day-01/index.test.js
@@ -1,11 +1,11 @@
 require("node:fs");
-const {parse, part1} = require("./index");
+const { parse, getFirstAndLastDigits, part1 } = require("./index");
 
 const rawTestData = "1abc2\npqr3stu8vwx\na1b2c3d4e5f\ntreb7uchet";
 
 jest.mock("node:fs", () => ({
-    readFileSync: jest.fn().mockReturnValue(rawTestData)
-}))
+    readFileSync: jest.fn().mockReturnValue(rawTestData),
+}));
 
 const testData = ["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"];
 
@@ -13,5 +13,18 @@ describe("data parsing", () => {
     it("parses .txt input into array of strings per line", () => {
         const parsedData = parse();
         expect(parsedData).toStrictEqual(testData);
+    });
+});
+
+describe("expected results", () => {
+    test("getFirstAndLastDigits", () => {
+        const expectedResults = [12, 38, 15, 77];
+        testData.forEach((line, idx) => {
+            expect(getFirstAndLastDigits(line)).toBe(expectedResults[idx]);
+        });
+    });
+    test("part1", () => {
+        const testSolution = part1(testData);
+        expect(testSolution).toBe(142);
     });
 });
