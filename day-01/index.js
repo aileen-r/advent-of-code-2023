4c0c4273fd20d5e934d5dd1be649eabef906b7ef
Aileen Rae <aileenlrae@gmail.com>
2023-12-01 21:26:39 +0000

    Basic data parse + editorconfig to make sure there are no pesky carriage returns

--- day-01/index.js
+++ day-01/index.js
@@ -1,1 +1,17 @@
-console.log("This is day 1");
+const fs = require("node:fs");
+
+function parse() {
+  try {
+    const data = fs.readFileSync("./day-01/test-input.txt", "utf8");
+    return data.split("\n");
+  } catch (err) {
+    console.error("Error reading input:", err);
+  }
+}
+
+function run() {
+  const data = parse();
+  console.log(data);
+}
+
+run();
